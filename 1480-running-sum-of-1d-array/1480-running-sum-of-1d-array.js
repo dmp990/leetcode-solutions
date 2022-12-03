d55ae4fa731b0129df95ec2ae8ac72779df49537
Asad Ali <42726454+dmp990@users.noreply.github.com>
2022-12-03 13:33:48 +0000

    Time: 60 ms (97.56%), Space: 42.3 MB (59.54%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
@@ -13,16 +13,4 @@ var runningSum = function(nums) {
     }
     
     return returnArr;
-    /*
-    const returnArray = [nums[0]];
-    
-    for (let i = 1; i < nums.length; i++) {
-        let sum = nums[i];
-        for (let j = 0; j < i; j++) {
-            sum += nums[j];
-        }
-        returnArray.push(sum);
-    }
-    return returnArray;
-    */
 };
