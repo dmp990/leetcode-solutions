f5e47b2c8fcf33a823558bf30e74c4568d2afcb7
Asad Ali <42726454+dmp990@users.noreply.github.com>
2022-12-04 11:07:11 +0000

    Time: 266 ms (9.22%), Space: 48.8 MB (48.45%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
@@ -2,10 +2,30 @@
  * @param {number[]} nums
  * @return {number[]}
  */
+
+// nums = [-4, -1, 0, 3, 10]
+// 2 pointers
+
 var sortedSquares = function(nums) {
+    // naive solution
+    /*
     for (let i = 0; i < nums.length; i++) {
         nums[i] = Math.pow(nums[i], 2);
     }
     nums.sort((a, b) => a - b);
     return nums;
+    */
+    // solution with two pointers
+    let resultArray = [];
+    let leftPtr = 0;
+    let rightPtr = nums.length - 1;
+    
+    nums.forEach(() => {
+        if (Math.abs(nums[leftPtr]) > Math.abs(nums[rightPtr])) {
+            resultArray.unshift(nums[leftPtr++] ** 2);
+        } else {
+            resultArray.unshift(nums[rightPtr--] ** 2);
+        }
+    })
+    return resultArray;
 };
