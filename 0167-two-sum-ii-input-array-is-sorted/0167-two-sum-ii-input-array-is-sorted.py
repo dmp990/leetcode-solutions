62b45b899dbe3975a6a51ca920924f495d61291e
Asad Ali <42726454+dmp990@users.noreply.github.com>
2022-12-05 09:31:18 +0000

    Time: 199 ms (73.22%), Space: 14.8 MB (99.41%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -6,7 +6,6 @@ class Solution:
         
         for i, n in enumerate(numbers):
             if target - n in hashmap:
-                # print(f"about to return {hashmap[target-n]} and {i}")
                 return [hashmap[target-n] + 1, i + 1]
             hashmap[n] = i
         
